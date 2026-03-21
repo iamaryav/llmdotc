5c4cd355ff33d0a4e8953f42a825b33ea26561fb
Priya Aryav <priya.aryav@gmail.com>
2026-03-21 23:00:00 +0530

    added kv cache

--- gpt.py
+++ gpt.py
@@ -29,6 +29,7 @@ def apply_rotary_emb(x, cos, sin):
 
     # half split
     x1, x2 = x[..., :d], x[..., d:]
+    cos, sin = cos[:,:,:x.shape[2],:], sin[:,:,:x.shape[2],:]
     y1 = x1 * cos + x2 * sin
     y2 = x1 * (-sin) + x2 * cos
 
@@ -37,6 +38,8 @@ def apply_rotary_emb(x, cos, sin):
 class CausalSelfAttention(nn.Module):
     def __init__(self, config):
         super().__init__()
+        self.cached_key = None
+        self.cached_value = None
         assert config.hidden_size % config.num_attention_heads == 0
         assert config.num_attention_heads % config.num_kv_heads == 0
         self.num_attention_heads = config.num_attention_heads
@@ -50,28 +53,38 @@ class CausalSelfAttention(nn.Module):
         self.register_buffer('tril', torch.tril(torch.ones(config.max_seq_len, config.max_seq_len)))
 
 
-    def forward(self, x, cos, sin):
+    def forward(self, x, cos, sin, kv_cache=False):
         # x -> (batch, max_seq_len, hidden_size)
         batch, seq_len = x.shape[:-1]
         # (batch, num_attention_heads, seq_len, head_dim)
         q = self.q_proj(x).view(batch, seq_len, self.num_attention_heads, self.head_dim).transpose(1, 2)
         k = self.k_proj(x).view(batch, seq_len, self.num_kv_heads, self.head_dim).transpose(1, 2)
         v = self.v_proj(x).view(batch, seq_len, self.num_kv_heads, self.head_dim).transpose(1, 2)
-        # expand the kv_heads to match query dimensions for calculation
-        k = k.expand(batch, self.num_attention_heads, seq_len, self.head_dim)
-        v = v.expand(batch, self.num_attention_heads, seq_len, self.head_dim)
 
-        # apply rotary embedding
+        if kv_cache:
+            if self.cached_key is None:
+                self.cached_key = k
+                self.cached_val = v
+            else:
+                k = torch.cat((self.cached_key, k), dim=2)
+                v = torch.cat((self.cached_val, v), dim=2)
+                self.cached_key = k
+                self.cached_val = v
+
+        repeat_factor = self.num_attention_heads // self.num_kv_heads
+        k = k.repeat_interleave(repeat_factor, dim=1)
+        v = v.repeat_interleave(repeat_factor, dim=1)
+
         q = apply_rotary_emb(q, cos, sin)
         k = apply_rotary_emb(k, cos, sin)
 
-        # QKNorm with rmsnorm
-        # rms norm to both query and key
-        q = norm(q) 
+        q = norm(q)
         k = norm(k)
 
         attn_wei = q @ k.transpose(-2, -1) * (k.shape[-1] ** -0.5)
-        attn_wei = attn_wei.masked_fill(self.tril[:seq_len,:seq_len] == 0, float("-inf"))
+        q_len, kv_len = q.shape[2], k.shape[2]
+        causal_mask = torch.tril(torch.ones(q_len, kv_len, device=q.device, dtype=torch.bool))
+        attn_wei = attn_wei.masked_fill(~causal_mask, float("-inf"))
         attn_wei = F.softmax(attn_wei, dim=-1)
         attn_wei = self.dropout(attn_wei)
 
@@ -105,9 +118,9 @@ class DecoderLayer(nn.Module):
         self.post_attention_norm = nn.LayerNorm(config.hidden_size)
         self.ffn = MLP(config)
 
-    def forward(self, x, cos, sin):
+    def forward(self, x, cos, sin, kv_cache=False):
         # pre-attention layernorm -> self attention -> post-attention layernorm -> ffn
-        x = x + self.self_attn(self.input_layernorm(x), cos, sin)
+        x = x + self.self_attn(self.input_layernorm(x), cos, sin, kv_cache=kv_cache)
         x = x + self.ffn(self.post_attention_norm(x))
         return x
 
@@ -142,18 +155,19 @@ class GPT(nn.Module):
     def _init_weights(self):
         pass
 
-    def forward(self, input_ids, target=None, device='cpu'):
+    def forward(self, input_ids, target=None, device='cpu', kv_cache=False):
 
         batch, seq_len = input_ids.shape
         tok_emb = self.embed_tokens(input_ids)
 
         # precompute rotary embeddings
         head_dim = self.config.hidden_size // self.config.num_attention_heads
-        cos, sin = self._precompute_rotary_embeddings(seq_len, head_dim, device=device)
+
+        cos, sin = self._precompute_rotary_embeddings(self.config.max_seq_len, head_dim, device=device)
 
         x = tok_emb
         for layer in self.layers:
-            x = layer(x, cos, sin)
+            x = layer(x, cos, sin, kv_cache=kv_cache)
         x = self.norm(x)
         logits = self.lm_head(x)
 
@@ -167,21 +181,33 @@ class GPT(nn.Module):
 
         return logits, loss
 
-    def generate(self, x, max_new_tokens=100):
+    def generate(self, x, max_new_tokens=100, temp=1.0):
         # list of tokens
         for _ in range(max_new_tokens):
-            x = x[:,-self.config.max_seq_len:] # B, T
+            cur_token = x[:,-self.config.max_seq_len:] # B, T
             # forward pass
-            logits, _ = self(x)
+            logits, _ = self(cur_token, kv_cache=True)
             # only last token matters
             logits = logits[:,-1,:]
             # probablity of all the tokens
+            if temp != 1.0:
+                logits = logits / temp
+
             probs = F.softmax(logits, dim=-1)
-            # temp?
             # token index
             idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+            cur_token = idx_next
             x = torch.cat((x, idx_next), dim=1) # (B, T+1)
         return x
+
+if __name__ == '__main__':
+    config = GPTConfig()
+    model = GPT(config)
+    torch.manual_seed(42)
+    print("hi")
+    x = torch.randint(1, 100, (1, 8))
+    tokens = model.generate(x, 10)
+    print(tokens)
         
 
 
