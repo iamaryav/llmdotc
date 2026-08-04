e9eb6b91ac811535a80a8e7b70e303d6fb72adba
Priya Aryav <priya.aryav@gmail.com>
2026-08-04 12:18:44 +0530

    refactor

---
+++ dev/cuda/layernorm_forward.cu
@@ -0,0 +1,18 @@
+// what is layer norm
+// normalize the layer
+// what math we do inside the layernorm
+
+
+void layer_norm_cpu(int* out, const int* inp, const int N){
+    // we will get the a matrix we have to normalize the valud on that matrix
+    // 
+}
+
+
+int main(int argc, char** argv){
+
+    return 0;
+}
+
+
+
